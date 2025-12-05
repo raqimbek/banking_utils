7a2c88b3bb53288d6cb08203fc1285018c160086
Raqimbek <raqimbek@proton.me>
2025-12-05 16:14:47 +0500

    refactored PaymentSystemDeterminerTest

--- core/src/test/java/dev/andrylat/raqimbek/bankingutils/core/cardutility/service/paymentsystemdeterminer/PaymentSystemDeterminerTest.java
+++ core/src/test/java/dev/andrylat/raqimbek/bankingutils/core/cardutility/service/paymentsystemdeterminer/PaymentSystemDeterminerTest.java
@@ -1,40 +1,42 @@
 package dev.andrylat.raqimbek.bankingutils.core.cardutility.service.paymentsystemdeterminer;
 
+import org.junit.jupiter.api.Test;
+
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PaymentSystemDeterminerTest {
-  PaymentSystemDeterminer paymentSystemDeterminer = new PaymentSystemDeterminer();
+    PaymentSystemDeterminer paymentSystemDeterminer = new PaymentSystemDeterminer();
 
-  //  @Test
-  //  public void shouldReturnVisa() {
-  //    var expected = "VISA";
-  //    var cardNumber = "4263 9826 4026 9299";
-  //    var actual = getPaymentSystem(cardNumber);
-  //
-  //    assertEquals(expected, actual);
-  //  }
-  //
-  //  @Test
-  //  public void shouldReturnMasterCard() {
-  //    var expected = "MASTERCARD";
-  //    var cardNumber = "5425 2334 3010 9903";
-  //    var actual = getPaymentSystem(cardNumber);
-  //
-  //    assertEquals(expected, actual);
-  //  }
-  //
-  //  @Test
-  //  public void shouldReturnEmptyString() {
-  //    var expected = "";
-  //    var cardNumber = "5863 9826 4026 9299";
-  //    var actual = getPaymentSystem(cardNumber);
-  //
-  //    assertEquals(expected, actual);
-  //  }
+    @Test
+    public void shouldReturnVisa() {
+        var expectedPaymentSystem = "VISA";
+        var cardNumber = new BigDecimal("4263982640269299");
 
-  private String getPaymentSystem(BigDecimal cardNumber) {
-    return paymentSystemDeterminer.determinePaymentSystem(cardNumber).toString();
-  }
+        assertPaymentSystem(cardNumber, expectedPaymentSystem);
+    }
+
+    @Test
+    public void shouldReturnMasterCard() {
+        var expectedPaymentSystem = "MASTERCARD";
+        var cardNumber = new BigDecimal("5425233430109903");
+
+        assertPaymentSystem(cardNumber, expectedPaymentSystem);
+    }
+
+    @Test
+    public void shouldReturnEmptyOptional() {
+        var cardNumber = new BigDecimal("5863982640269299");
+
+        assertFalse(paymentSystemDeterminer.determinePaymentSystem(cardNumber).isPresent());
+
+    }
+
+    private void assertPaymentSystem(BigDecimal cardNumber, String expectedPaymentSystem) {
+        var optionalPaymentSystem = paymentSystemDeterminer.determinePaymentSystem(cardNumber);
+
+        assertTrue(optionalPaymentSystem.isPresent());
+        assertEquals(expectedPaymentSystem, optionalPaymentSystem.get().toString());
+    }
 }
