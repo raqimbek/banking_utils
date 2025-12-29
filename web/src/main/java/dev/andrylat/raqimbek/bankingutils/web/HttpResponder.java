f59522d51a57fde358bea1f765fbeeebac06965a
Raqimbek <raqimbek@proton.me>
2025-12-29 20:29:31 +0500

    add standard encoding for the response

--- web/src/main/java/dev/andrylat/raqimbek/bankingutils/web/HttpResponder.java
+++ web/src/main/java/dev/andrylat/raqimbek/bankingutils/web/HttpResponder.java
@@ -4,6 +4,7 @@ import com.sun.net.httpserver.HttpExchange;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class HttpResponder {
   private void respond(HttpExchange exchange, String response, int statusCode) throws IOException {
@@ -11,7 +12,7 @@ public class HttpResponder {
 
     exchange.sendResponseHeaders(statusCode, response.length());
 
-    outputStream.write(response.getBytes());
+    outputStream.write(response.getBytes(StandardCharsets.UTF_8));
     outputStream.close();
   }
 
