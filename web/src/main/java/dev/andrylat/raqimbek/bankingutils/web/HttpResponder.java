2b588ed7f3d97f208e39638a4e4ce14173d42ce2
Raqimbek <raqimbek@proton.me>
2026-01-04 05:40:50 +0500

    add required response headers

--- web/src/main/java/dev/andrylat/raqimbek/bankingutils/web/HttpResponder.java
+++ web/src/main/java/dev/andrylat/raqimbek/bankingutils/web/HttpResponder.java
@@ -7,20 +7,21 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class HttpResponder {
-  private void respond(HttpExchange exchange, String response, int statusCode) throws IOException {
-    var outputStream = exchange.getResponseBody();
+    private void respond(HttpExchange exchange, String response, int statusCode) throws IOException {
+        var outputStream = exchange.getResponseBody();
 
-    exchange.sendResponseHeaders(statusCode, response.length());
+        exchange.sendResponseHeaders(statusCode,response.getBytes().length);
 
-    outputStream.write(response.getBytes(StandardCharsets.UTF_8));
-    outputStream.close();
-  }
+        outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+    }
 
-  public void respondJson(HttpExchange exchange, JSONObject responseDataJson, int statusCode)
-      throws IOException {
+    public void respondJson(HttpExchange exchange, JSONObject responseDataJson, int statusCode)
+            throws IOException {
 
-    exchange.getResponseHeaders().set("Content-Type", "application/json; charset=utf-8");
+        var responseHeaders = exchange.getResponseHeaders();
 
-    respond(exchange, responseDataJson.toString(), statusCode);
-  }
+        responseHeaders.set("Content-Type", "application/json; charset=utf-8");
+
+        respond(exchange, responseDataJson.toString(), statusCode);
+    }
 }
